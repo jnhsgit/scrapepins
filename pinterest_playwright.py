8650e7446dc7c422804cc6e23a68f7d1dee8bd28
Alex <alexander.toland33@gmail.com>
2024-05-30 23:23:07 +1000

    getting closer

--- pinterest_playwright.py
+++ pinterest_playwright.py
@@ -1,24 +1,31 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+from bs4 import BeautifulSoup
 import time
 
-#this is just random testing. 
-#I was able to generate this template by running playwright codegen on CLI
-#See: https://playwright.dev/python/docs/codegen#running-codegen
+def extract_alt_text(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    img_tags = soup.find_all('img', alt=True)
+    alt_texts = [img.get('alt', '') for img in img_tags]
+    return alt_texts
+
+#great success - plan is to add img alts to a list and then iterate through them
+# with a function that clicks into them, downloads the image and then resets
+# the local storage unauth download to 0 (so we can have unlimited downloads)
+# also want to simulate scrolling down so we can load more images - because
+# pinterest has lazy loading.
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.pinterest.com.au/search/pins/?q=2%20tier%20wedding%20cake%20blue")
     page.wait_for_load_state()
+    time.sleep(5)
+    content = page.content()
+    alt_texts = extract_alt_text(content)
+    print(alt_texts)
+    time.sleep(200)
     #using time.sleep to encourage more loading since wait_for_load_state doesn't seem sufficient
-    time.sleep(10)
-    page.locator("a").filter(has_text="Openthebridesofoklahoma.com;").click()
-    with page.expect_download() as download_info:
-        page.locator("[data-test-id=\"more-options-download\"]").get_by_role("button", name="Download image").click(button="right")
-    download = download_info.value
-    page.goto("https://www.pinterest.com.au/search/pins/?q=2%20tier%20wedding%20cake%20blue")
 
-    # ---------------------
     context.close()
     browser.close()
 
